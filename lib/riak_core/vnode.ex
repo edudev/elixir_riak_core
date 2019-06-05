b45a681dce1a38914ea8b3a165626d7a862f9a1d
Emil Dudev <e.dudev@drooms.com>
2019-06-05 12:47:50 +0300

    Add documentation for the bare vnode

--- lib/riak_core/vnode.ex
+++ lib/riak_core/vnode.ex
@@ -1,54 +1,155 @@
 defmodule RiakCore.VNode do
   alias RiakCore.VNode
 
-  @type state_data() :: term()
+  @typedoc """
+  A term representing the current state of the vnode.
+  """
+  @type state_vnode() :: term()
+
+  @typedoc """
+  A term representing any error that might occur.
+  """
   @type error_reason() :: term()
 
+  @doc """
+  Invoked when the vnode is started. `start_link/2` will block until it returns.
 
-  @callback init() :: {:ok, state_data()} | {:error, error_reason()}
-  @callback terminate(error_reason(), state_data()) :: any()
+  There are no arguments.
+
+  Returning `{:ok, data}` will cause `start_link/2` to return `{:ok, pid}` and the process to enter its loop.
+
+  Returning `:ignore` will cause `start_link/2` to return `:ignore` and the
+  process will exit normally without entering the loop or calling `terminate/2`.
+  If used when part of a supervision tree the parent supervisor will not fail
+  to start nor immediately try to restart the `VNode`. The remainder
+  of the supervision tree will be (re)started and so the `VNode`
+  should not be required by other processes. It can be started later with
+  `Supervisor.restart_child/2` as the child specification is saved in the parent
+  supervisor. The main use cases for this are:
+    * The `VNode` is disabled by configuration but might be enabled
+      later.
+    * An error occurred and it will be handled by a different mechanism than the
+     `Supervisor`. Likely this approach involves calling
+     `Supervisor.restart_child/2` after a delay to attempt a restart.
+
+  Returning `{:error, reason}` will cause `start_link/2` to return
+  `{:error, reason}` and the process to exit with reason `reason` without
+  entering the loop or calling `terminate/2`.
+  """
+  @callback init() :: {:ok, state :: state_vnode()} | {:error, reason :: error_reason()}
+
+  @doc """
+  Invoked when the vnode is about to exit. It should do any cleanup required.
+
+  `reason` is exit reason, `state` is the current state of the `VNode`.
+   The return value is ignored.
+
+  `terminate/2` is called if a callback (except `init/1`) returns a `:stop`
+  tuple, raises, calls `Kernel.exit/1` or returns an invalid value. It may also
+  be called if the `VNode` traps exits using `Process.flag/2` *and*
+  the parent process sends an exit signal.
+
+  If part of a supervision tree a `VNode`'s `Supervisor` will send an
+  exit signal when shutting it down. The exit signal is based on the shutdown
+  strategy in the child's specification. If it is `:brutal_kill` the
+  `VNode` is killed and so `terminate/2` is not called. However if it
+  is a timeout the `Supervisor` will send the exit signal `:shutdown` and the
+  `VNode` will have the duration of the timeout to call `terminate/2`
+  - if the process is still alive after the timeout it is killed.
+
+  If the `VNode` receives an exit signal (that is not `:normal`) from
+  any process when it is not trapping exits it will exit abruptly with the same
+  reason and so not call `terminate/2`. Note that a process does *NOT* trap
+  exits by default and an exit signal is sent when a linked process exits or its
+  node is disconnected.
+
+  Therefore it is not guaranteed that `terminate/2` is called when a
+  `VNode` exits. For such reasons, we usually recommend important
+  clean-up rules to happen in separated processes either by use of monitoring or
+  by links themselves. For example if the `VNode` controls a `port`
+  (e.g. `:gen_tcp.socket`) or `File.io_device`, they will be closed on receiving
+  a `VNode`'s exit signal and does not need to be closed in `terminate/2`.
+
+  If `reason` is not `:normal`, `:shutdown` nor `{:shutdown, term}` an error is
+  logged.
+
+  This function can optionally throw a result, which is ignored.
+  """
+  @callback terminate(reason :: error_reason(), state :: state_vnode()) :: any()
 
   defmacro __using__(_) do
     quote location: :keep do
       @behaviour VNode
 
-      def terminate(_reason, _state), do: :ok
+      def terminate(_reason, _state_fsm), do: :ok
 
-      defoverridable [terminate: 2]
+      defoverridable terminate: 2
     end
   end
 
+  # "protected" API
 
-  # public API
+  @doc """
+  Starts a `VNode` process linked to the current process.
 
-  @spec start_link(module()) :: GenServer.on_start()
-  def start_link(module) do
-    GenStateMachine.start_link(__MODULE__.StateMachine, module)
+  This is often used to start the `VNode` as part of a supervision tree.
+
+  Once the server is started, the `init/1` function of the given `module` is
+  called to initialize the server. To ensure a synchronized start-up procedure,
+  this function does not return until `init/1` has returned.
+
+  Note that a `VNode` started with `start_link/2` is linked to the
+  parent process and will exit in case of crashes from the parent. The
+  `VNode` will also exit due to the `:normal` reasons in case it is
+  configured to trap exits in the `init/1` callback.
+
+  ## Options
+    * `:timeout` - if present, the server is allowed to spend the given amount of
+      milliseconds initializing or it will be terminated and the start function
+      will return `{:error, :timeout}`
+    * `:debug` - if present, the corresponding function in the [`:sys`
+      module](http://www.erlang.org/doc/man/sys.html) is invoked
+    * `:spawn_opt` - if present, its value is passed as options to the
+      underlying process as in `Process.spawn/4`
+
+  ## Return values
+
+  If the vnode is successfully created and initialized, this function returns
+  `{:ok, pid}`, where `pid` is the pid of the server.
+
+  If the `init/1` callback fails with `reason`, this function returns
+  `{:error, reason}`. Otherwise, if it returns `{:stop, reason}`
+  or `:ignore`, the process is terminated and this function returns
+  `{:error, reason}` or `:ignore`, respectively.
+  """
+  @spec start_link(module(), GenServer.options()) :: GenServer.on_start()
+  def start_link(module, options \\ []) do
+    GenStateMachine.start_link(__MODULE__.StateMachine, module, options)
   end
 
   # state machine
   defmodule StateMachine do
+    @moduledoc false
     use GenStateMachine, callback_mode: :state_functions
 
     require Record
-    Record.defrecordp(:state, [:module, :data])
+    Record.defrecordp(:state_fsm, [:module, :data])
 
     @typep states :: :active
-    @typep state :: record(:state, module: module(), data: VNode.state_data())
-
+    @typep state_fsm :: record(:state_fsm, module: module(), data: VNode.state_vnode())
 
     @spec init(module()) :: :gen_statem.init_result(states())
     def init(module) do
       case module.init() do
-        {:ok, state_data} -> {:ok, :active, state(module: module, data: state_data)}
+        {:ok, state_vnode} -> {:ok, :active, state_fsm(module: module, data: state_vnode)}
         {:error, reason} -> {:stop, reason}
         :ignore -> :ignore
       end
     end
 
-    @spec terminate(VNode.error_reason(), states(), state()) :: any()
-    def terminate(reason, _state_name, state(module: module, data: state_data)) do
-      module.terminate(reason, state_data)
+    @spec terminate(VNode.error_reason(), states(), state_fsm()) :: any()
+    def terminate(reason, _state_name, state_fsm(module: module, data: state_vnode)) do
+      module.terminate(reason, state_vnode)
     end
   end
 end
--- mix.exs
+++ mix.exs
@@ -23,8 +23,8 @@ defmodule RiakCore.MixProject do
   defp deps do
     [
       {:gen_state_machine, "~> 2.0"},
-
-      {:dialyxir, "~> 0.5", only: [:dev]}
+      {:dialyxir, "~> 0.5", only: [:dev]},
+      {:ex_doc, "~> 0.20", only: [:dev]}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
